8fb0fc7a20e2a8179b6bc85420d031449e48ae46
Maria <marirg02@hotmail.com>
2025-11-09 19:02:16 -0600

    git commit

--- app.py
+++ app.py
@@ -5,10 +5,8 @@ import plotly.graph_objects as go
  #Leer los datos del archivo CSV
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header('Análisis exploratorio vehículos en US')
+st.header('Análisis exploratorio para el conjunto de datos de anuncios de venta de coches')
 
-# Crear un botón en la aplicación Streamlit
-hist_button = st.button('Construir histograma')
 
 # crear una casilla de verificación
 build_histogram = st.checkbox('Construir un histograma')
@@ -32,9 +30,9 @@ if build_histogram: # si la casilla de verificación está seleccionada
 disp_button = st.button('Construir gráfico de dispersón')
 
 # Lógica a ejecutar cuando se hace clic en el botón
-if hist_button:
+if disp_button:
     # Escribir un mensaje en la aplicación
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     # Crear un scatter plot utilizando plotly.graph_objects
     # Se crea una figura vacía y luego se añade un rastro de scatter
     fig = go.Figure(data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
